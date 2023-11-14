560592f016e9fde75d688a9887c0916b381c227d
MOPSKATER <josua.w@gmx.de>
2023-11-14 12:10:44 +0100

    Projektdateien hinzufügen.

---
+++ DiceBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using Discord.WebSocket;
+using Discord;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+
+    private DiscordSocketClient? _client;
+
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = "token";
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+}
