f91a45ae2944e6e5d85eb8ee1e493655291a53fb
MOPSKATER <MOPSKATER@users.noreply.github.com>
2023-11-21 15:28:51 +0100

    Working version

---
+++ DiceBot/Dicer.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DiceBot
+{
+    internal class Dicer
+    {
+        private Random _random = new Random();
+        private const string PATTERN = "^[0-9]{1,3}d[0-9]{1,3}$";
+
+        public string RollDice(string diceString)
+        {
+            Match match = Regex.Match(diceString, PATTERN);
+            if (!match.Success) return "Your string does not match following pattern " + PATTERN;
+
+            string[] parameters = diceString.Split('d');
+            int amount = int.Parse(parameters[0]);
+            int diceType = int.Parse(parameters[1]);
+
+            if (amount < 1) return "You must roll at lease 1 dice";
+            if (diceType < 2) return "You must roll at least a dice with 2 faces";
+
+            int sum = 0, successes = 0;
+            int[] values = new int[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                int value = _random.Next(1, diceType + 1);
+                values[i] = value;
+                sum += value;
+
+                if (value == 1)
+                    successes--;
+                else if (value == diceType)
+                    successes += 2;
+                else if (value >  diceType / 2 + 1)
+                    successes++;
+            }
+
+            return string.Format("∑: {0}\n✓: {1}\n({2} [{3}])", sum, successes, diceString, string.Join(' ', values));
+        }
+    }
+}
--- DiceBot/Program.cs
+++ DiceBot/Program.cs
@@ -1,54 +1,74 @@
 ﻿using Discord.WebSocket;
 using Discord;
 using Discord.Net;
-using Newtonsoft.Json;
+using System.Diagnostics;
+using DiceBot;
 
 public class Program
 {
     public static Task Main(string[] args) => new Program().MainAsync(args[0]);
 
 
-    private DiscordSocketClient? _client;
+    private DiscordSocketClient _client = new();
+    private Dicer _dicer = new();
 
     public async Task MainAsync(string token)
     {
-        _client = new DiscordSocketClient();
-        var guild = _client.GetGuild(1173980055387512902);
-
-        _client.Log += Log;
-        SlashCommandBuilder rollCommandBuilder = new SlashCommandBuilder()
-            .WithName("r")
-            .WithDescription("Rolls a dice (5d10 rolls 5 d10 dices)");
-
-
-        _client.MessageReceived += MsgRcv;
+        _client.Ready += Ready;
+        _client.SlashCommandExecuted += SlashCommandHandler;
 
         await _client.LoginAsync(TokenType.Bot, token);
-
-        try
-        {
-            await guild.CreateApplicationCommandAsync(rollCommandBuilder.Build());
-        }
-        catch (HttpException exception)
-        {
-            Console.WriteLine(exception.ToString());
-        }
-
         await _client.StartAsync();
 
         // Block this task until the program is closed.
         await Task.Delay(-1);
     }
 
-    private Task Log(LogMessage msg)
+    private async Task Ready()
     {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
+        var guild = _client.GetGuild(1173980055387512902);
+
+        SlashCommandBuilder rollCommandBuilder = new SlashCommandBuilder()
+            .WithName("r")
+            .WithDescription("Rolls a dice (5d10 rolls 5 d10 dices)")
+            .AddOption("dice", ApplicationCommandOptionType.String, "<Dice amount>d<Dice type>", isRequired: true);
+
+        SlashCommandBuilder eRollCommandBuilder = new SlashCommandBuilder()
+            .WithName("er")
+            .WithDescription("Ephemeral roll only responds the command sender")
+            .AddOption("dice", ApplicationCommandOptionType.String, "<Dice amount>d<Dice type>", isRequired: true);
+
+        try
+        {
+            await guild.CreateApplicationCommandAsync(rollCommandBuilder.Build());
+            await guild.CreateApplicationCommandAsync(eRollCommandBuilder.Build());
+            //await _client.CreateGlobalApplicationCommandAsync(rollCommandBuilder.Build());
+            //await _client.CreateGlobalApplicationCommandAsync(eRollCommandBuilder.Build());
+        }
+        catch (HttpException exception)
+        {
+            Console.WriteLine(exception.ToString());
+            Environment.Exit(1);
+        }
+        Console.WriteLine("Bot ready");
     }
 
-    private Task MsgRcv(SocketMessage msg)
+    private async Task SlashCommandHandler(SocketSlashCommand command)
     {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
+        switch (command.Data.Name)
+        {
+            case "r": 
+                await RollDices(command, false);
+                break;
+            case "er":
+                await RollDices(command, true);
+                break;
+        }
+    }
+
+    private async Task RollDices(SocketSlashCommand command, bool ephemeral)
+    {
+        string diceString = (string)command.Data.Options.First().Value;
+        await command.RespondAsync(_dicer.RollDice(diceString), ephemeral: ephemeral);
     }
 }
